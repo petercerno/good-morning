01efc0fcbcae9ff349f1722852de09d30bb6251a
Linwood Creekmore III <valinvescap@gmail.com>
2017-06-13 07:24:15 -0400

    Made files to pip install; still need to make unittests.

--- setup.py
+++ setup.py
@@ -1,111 +1,68 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+import codecs
+import os
+import re
 
-here = path.abspath(path.dirname(__file__))
+from setuptools import setup
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(filename):
+    with codecs.open(os.path.join(cwd, filename), 'rb', 'utf-8') as h:
+        return h.read()
+
+
+metadata = read(os.path.join(cwd, 'morningstar', '__init__.py'))
+
+
+def extract_metaitem(meta):
+    # swiped from https://hynek.me 's attr package
+    meta_match = re.search(
+        r"""^__{meta}__\s+=\s+['\"]([^'\"]*)['\"]""".format(meta=meta),
+        metadata, re.MULTILINE)
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError('Unable to find __{meta}__ string.'.format(meta=meta))
+
 
 setup(
-    name='good-morning',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
-
-    description='Simple Python module for downloading fundamental financial data from financials.morningstar.com.',
-    long_description=long_description,
-    
-    # The project's main homepage.
-    url='https://github.com/petercerno/good-morning',
-
-    # Author details
-    author='Peter Cerno',
-    #author_email='pypa-dev@googlegroups.com',
-
-    # Choose your license
-    license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    name='morningstar',
+    version=extract_metaitem('version'),
+    license=extract_metaitem('license'),
+    description=extract_metaitem('description'),
+    long_description=(read('README.md') + '\n\n' +
+                      read('AUTHORS.rst') + '\n\n' +
+                      read('CHANGES')),
+    author=extract_metaitem('author'),
+    author_email=extract_metaitem('email'),
+    maintainer=extract_metaitem('author'),
+    maintainer_email=extract_metaitem('email'),
+    url=extract_metaitem('url'),
+    # download_url=extract_metaitem('download_url'),
+    platforms=['Any'],
+    packages=['morningstar'],
+    install_requires=['numpy', 'pandas', 'pymysql',
+                      'python-dateutil','beautifulsoup4',
+                      'mock;python_version<"3.3"',
+                      "futures; python_version < '3.0'",
+                      "futures>=3.0.5; python_version == '2.6' or python_version=='2.7'"
+                      ],
+    keywords='stocks morningstar financial data historical',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers', 'Financial analysts',
-        'Topic :: Securities Analysis :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License : MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Office/Business :: Financial',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5'
     ],
-
-    # What does your project relate to?
-    keywords='financial stock morningstar analysis historical data',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['beautifulsoup4', 'numpy', 'pandas','pymysql','urllib']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['beautifulsoup4>=4.4.1','numpy>=1.10.0','pandas>=0.17.0','pymysql>=0.6.7','re>=1.3.3','urllib>=0.1.13'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    #extras_require={
-        #'dev': ['check-manifest'],
-        #'test': ['coverage'],},
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-        #'sample': ['package_data.dat'],
-    #},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-        #'console_scripts': [
-            #'sample=sample:main',
-        #],
-    #},
 )
